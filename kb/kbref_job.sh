53190e6ffdeaebda1f8e33da0eb51a5ed558be91
JunyiCHEN <junyichen@d24h.hk>
2021-03-12 14:56:05 +0800

    Edit the parameter inputs of kb script

--- kb/kbref_job.sh
+++ kb/kbref_job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo input path $1;
-echo output path $2;
+echo fasta path $1;
+echo gtf path $2;
+echo output path $3;
 kb ref \
--i=$2 \
--f1=$1 \
--g=$2 \
+-i=$3 \
+-g=$3 \
+$1 $2
--- kb/submkref_jobs.sh
+++ kb/submkref_jobs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
   echo "$#";
-  echo "Usage: $0 <Fasta_directory> <output_directory>" >&2
+  echo "Usage: $0 <Fasta_directory> <gtf_directory> <output_directory>" >&2
   exit 1
 fi
 timestamp=$(date +%m%d%Y%H%M);
 fastapath=$1;
-outpath=$2;
+gtfpath=$2;
+outpath=$3;
+
 #rm -r human_host_viruses.viruSITE.with_hg38
-nohup /usr/bin/time -v ./kbref_job.sh $fastapath $outpath > ../output/kbref_$timestamp.out 2>&1
+nohup /usr/bin/time -v ./kbref_job.sh $fastapath $gtfapath $outpath > ../output/kbref_$timestamp.out 2>&1
