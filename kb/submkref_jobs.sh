95a939f5b0ffd70af87be1f848360765e4893087
JunyiCHEN <junyichen@d24h.hk>
2021-03-12 14:40:09 +0800

    Edit to just detect the number of parameters

--- kb/submkref_jobs.sh
+++ kb/submkref_jobs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ "$#" -ne 2 ] || ! [ -d "$1" ] || ! [ -d "$2" ]; then
+if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <Fasta_directory> <output_directory>" >&2
   exit 1
 fi
-timestamp=$(date +%m%d%Y%H%M)
-fastapath=$1
-outpath=$2
+timestamp=$(date +%m%d%Y%H%M);
+fastapath=$1;
+outpath=$2;
 #rm -r human_host_viruses.viruSITE.with_hg38
 nohup /usr/bin/time -v ./kbref_job.sh $fastapath $outpath > output/kbref_$timestamp.out 2>&1
