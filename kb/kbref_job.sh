8df974202cbdf3b522f40ea1124fb02482683a37
JunyiCHEN <junyichen@d24h.hk>
2021-03-12 15:21:55 +0800

    Edit the script of kb

--- kb/kbref_job.sh
+++ kb/kbref_job.sh
@@ -3,6 +3,7 @@ echo fasta path $1;
 echo gtf path $2;
 echo output path $3;
 kb ref \
--i=$3 \
--g=$3 \
--f1=$1 $2
+-i=$3/transcriptome.idx \
+-g=$3/transcripts_to_genes.txt \
+-f1=$3/cdna.fa \
+$1 $2
