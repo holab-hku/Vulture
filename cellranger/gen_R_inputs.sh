456a5130e1f952ea829dc51357450507a58a9b00
JunyiCHEN <junyichen@d24h.hk>
2021-03-12 10:46:22 +0800

    Add filtered matrix in the gen R input script

--- cellranger/gen_R_inputs.sh
+++ cellranger/gen_R_inputs.sh
@@ -10,8 +10,11 @@ echo $3;
 # mkdir $1/STARsolo_outs
 # mkdir $1/STARsolo_outs/Gene
 mkdir -p $1/STARsolo_outs/Solo.out/Gene/raw
+mkdir -p $1/STARsolo_outs/Solo.out/Gene/filtered_feature_bc_matrix
 
 cp $3/outs/raw_feature_bc_matrix/*gz $1/STARsolo_outs/Solo.out/Gene/raw
+cp $3/outs/filtered_feature_bc_matrix/*gz $1/STARsolo_outs/Solo.out/Gene/filtered_feature_bc_matrix
+
 cp -r $2 $1
 
 
