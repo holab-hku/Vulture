d4bcce0417257feddc38877e75f64d669f839405
jholab <winghare@gmail.com>
2021-05-03 17:23:13 +0800

    add alevin

--- cellranger/count_job.sh
+++ cellranger/count_job.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 echo $1
 cellranger count \
---id=run_covid_host \
---fastqs=/storage/holab/covid_scRNA-seq/$1 \
+--id=run_covid_vmhost \
+--fastqs=/home/jholab/data/COV19/SRR11537949 \
 --sample=SRR11537949 \
---transcriptome=human_host_viruses.viruSITE.with_hg38
+--localcores = 10 \
+--include-introns \
+--transcriptome=/home/jholab/data/vmh_genome_dir/references/human_host_viruses_microbes.viruSITE.NCBIprokaryotes.with_hg38 & \
+cellranger count \
+--id=run_covid_vmhost \
+--fastqs=/home/jholab/data/COV19/SRR11537950 \
+--sample=SRR11537950 \
+--localcores = 10 \
+--include-introns \
+--transcriptome=/home/jholab/data/vmh_genome_dir/references/human_host_viruses_microbes.viruSITE.NCBIprokaryotes.with_hg38 & \
+cellranger count \
+--id=run_covid_vmhost \
+--fastqs=/home/jholab/data/COV19/SRR11537951 \
+--sample=SRR11537951 \
+--localcores = 10 \
+--include-introns \
+--transcriptome=/home/jholab/data/vmh_genome_dir/references/human_host_viruses_microbes.viruSITE.NCBIprokaryotes.with_hg38;
