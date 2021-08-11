8e7ba3d68907916eed37e5a36f1236a1eeee0b5c
WINGHARE <winghare@gmail.com>
2021-08-11 16:15:37 +0800

    Updata dependencies

--- r/scvh_dependencies.r
+++ r/scvh_dependencies.r
@@ -4,6 +4,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE)){
 if (!requireNamespace("remotes", quietly = TRUE)) {
   install.packages("remotes")
 }
+install.packages("argparse")
 BiocManager::install("DropletUtils",Ncpus=10)
 BiocManager::install(c("Biostrings", "ShortRead","doParallel","GenomicAlignments","Gviz","GenomicFeatures","Rsubread"),Ncpus=10)
 remotes::install_github("mojaveazure/seurat-disk",upgrade="always",Ncpus=10)
